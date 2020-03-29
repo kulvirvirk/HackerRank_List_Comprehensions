f48da75a2ebfe73e75ba462e16bd5f3341c39da4
kulvirvirk <kvirkdev@gmail.com>
2020-03-29 19:24:45 +0000

    added list example

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 # Problem:
 # https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+
+# still need to work on this problem
+
+
+squares_list = []
+
+for i in range(1,11):
+  squares_list.append(i*i)
+
+print(squares_list)
+
+#==================================
+
