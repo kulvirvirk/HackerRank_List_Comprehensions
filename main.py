f7800b9f7376130a16c331ae90e243e7cf51cc71
kulvirvirk <kvirkdev@gmail.com>
2020-03-29 19:25:06 +0000

    added map() example

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ print(squares_list)
 
 #==================================
 
+items_cost_list = [2.11, 4.10, 99.2, 54.90, .99 ]
+tax_rate = .08
+
+def add_tax_to_item(items_cost_list):
+ return items_cost_list * (1+tax_rate)
+
+total_after_tax = map(add_tax_to_item, items_cost_list)
+
+print(list(total_after_tax))
